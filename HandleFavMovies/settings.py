89be723f0099bdbfcd6c97caaf8a644b0e57e131
BHAVESH KUMAR <bhaveshsiwana2004@gmail.com>
2026-04-24 23:06:13 +0530

    remove some errors

--- HandleFavMovies/settings.py
+++ HandleFavMovies/settings.py
@@ -124,6 +124,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # WhiteNoise static file compression
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
